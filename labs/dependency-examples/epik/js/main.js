bf92ac1f45f8311262b68181f898d96fc7deec16
DimitarChristoff <christoff@gmail.com>
2014-01-20 15:39:23 +0000

    latest epik and static build

--- labs/dependency-examples/epik/js/main.js
+++ labs/dependency-examples/epik/js/main.js
@@ -1,13 +1,28 @@
 require.config({
 	paths: {
 		epik: '../bower_components/epik/lib',
+		'rivets-adapter': '../bower_components/epik/lib/plugins/rivets-adapter',
 		primish: '../bower_components/primish',
 		lodash: '../bower_components/lodash/dist/lodash',
 		slicker: '../bower_components/slicker/index',
 		rivets: '../bower_components/rivets/dist/rivets',
 		jquery: '../bower_components/jquery/jquery',
-		'rivets-adapter': 'epik/plugins/rivets-adapter',
 		text: '../bower_components/requirejs-text/text'
+	},
+	bundlesss: {
+		'epik/epik-min': [
+			'epik/index',
+			'epik/model',
+			'epik/model-sync',
+			'epik/collection',
+			'epik/collection-sync',
+			'epik/agent',
+			'epik/storage',
+			'epik/router',
+			'epik/view',
+			'epik/plugins/rivets-adapter',
+			'slicker'
+		]
 	}
 	//urlArgs: 'b=' + +new Date
 });
@@ -15,6 +30,8 @@ require.config({
 
 define(function(require){
 	'use strict';
+
+	require('epik/epik-min');
 
 	var TodoCollection = require('collections/todo-collection'),
 		TodoList = require('views/todo-list-view'),
